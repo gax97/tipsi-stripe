4c2ce2881331c3b411ae322238957f76e778d23a
danilofatcat <danilo.gacevic@fatcatcoders.com>
2021-03-22 13:52:57 +0100

    add

---
+++ ios/TPSStripe/StripeSdk-Bridging-Header.h
@@ -0,0 +1,20 @@
+//
+//  StripeSdk-Bridging-Header.h
+//  TPSStripe
+//
+//  Created by Danilo Gacevic on 22.3.21..
+//  Copyright © 2021 Tipsi. All rights reserved.
+//
+
+#ifndef StripeSdk_Bridging_Header_h
+#define StripeSdk_Bridging_Header_h
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTView.h>
+#import <React/RCTBridge.h>
+#import <React/RCTUIManager.h>
+#import <PassKit/PassKit.h>
+#import <React/RCTEventEmitter.h>
+
+#endif /* StripeSdk_Bridging_Header_h */
---
+++ ios/TPSStripe/TPSStripe-Bridging-Header.h
@@ -0,0 +1,19 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//
+//  StripeSdk-Bridging-Header.h
+//  TPSStripe
+//
+//  Created by Danilo Gacevic on 22.3.21..
+//  Copyright © 2021 Tipsi. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTView.h>
+#import <React/RCTBridge.h>
+#import <React/RCTUIManager.h>
+#import <PassKit/PassKit.h>
+#import <React/RCTEventEmitter.h>
